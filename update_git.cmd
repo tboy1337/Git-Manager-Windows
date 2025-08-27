b08c60081306f9688bb2a84f5cd4919f3db2f843
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-08-27 13:54:34 +0100

    Update exit codes in update_git.cmd for improved error handling, changing exit codes for user privilege checks and Git installation verification to provide clearer feedback on script execution status.

--- update_git.cmd
+++ update_git.cmd
@@ -5,14 +5,14 @@ net session >nul 2>&1
 if %errorlevel% equ 0 (
     echo This script is intended for per-user installation. Please run without administrator privileges.
     timeout /t 5 /nobreak
-    exit /b 1
+    exit /b 3
 )
 
 where git >nul 2>&1
 if %errorlevel% neq 0 (
     echo Git is not installed or in PATH.
     timeout /t 5 /nobreak
-    exit /b 2
+    exit /b 4
 )
 
 echo Updating Git using built-in updater...
